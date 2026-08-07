75079dc0deaf55ab0a69c8d19d25dd12de5f45e9
Umesh <umesh.ssagar035@gmail.com>
2026-08-07 14:12:30 +0530

    Add invoice service and controller

---
+++ backend-.NET/Vehiclecfg_group9/Vehiclecfg_group9/DTO/InvoiceCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vehiclecfg_group9.DTO
+{
+    public class InvoiceCreateDto
+    {
+        public DateTime? InvDate { get; set; }
+
+        public double? NetAmt { get; set; }
+
+        public double? Tax { get; set; }
+
+        public double? TotalAmt { get; set; }
+
+        public int ModelId { get; set; }
+
+        public int Id { get; set; }
+    }
+}
--- backend-.NET/Vehiclecfg_group9/Vehiclecfg_group9/Program.cs
+++ backend-.NET/Vehiclecfg_group9/Vehiclecfg_group9/Program.cs
@@ -1,4 +1,3 @@
-
 using Vehiclecfg_group9.data;
 using Vehiclecfg_group9.Models;
 using Vehiclecfg_group9.Repository;
@@ -15,31 +14,63 @@ namespace Vehiclecfg_group9
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            // Database Context
             builder.Services.AddDbContext<VehicleCfgContext>();
+
+
+            // =========================
+            // Existing Services
+            // =========================
 
             builder.Services.AddScoped<SegmentService>();
             builder.Services.AddScoped<MfgService>();
+
+
+            // =========================
+            // Generic Repository
+            // =========================
 
             builder.Services.AddScoped<IGenericRepository<SegmentMaster>,
                                        GenericRepository<SegmentMaster>>();
 
             builder.Services.AddScoped<IGenericRepository<MfgMaster>,
                                        GenericRepository<MfgMaster>>();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+            // =========================
+            // Invoice Service
+            // =========================
+
+            builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+
+
+            // =========================
+            // Invoice uses Generic Repository
+            // =========================
+
+            builder.Services.AddScoped<IGenericRepository<Invoice>,
+                                       GenericRepository<Invoice>>();
+
+
+            // OpenAPI
             builder.Services.AddOpenApi();
+
 
             var app = builder.Build();
 
+
             // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
 
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
